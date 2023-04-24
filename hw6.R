80312d430587241a777c0694b944a83950eb1ec0
cdlesage <cdl23@live.unc.edu>
2023-04-23 22:54:22 -0400

    question 3

--- hw6.R
+++ hw6.R
@@ -15,6 +15,20 @@ state_counts = group_by(data, State) %>%
   summarize(count= n())
 
 ## Question 2
+## Filtering data only for observations in NC and SC
 NCandSC = filter(data, State == "NC"| State == "SC")
-
+## Viewing the cities in these states
 NCandSC$City
+
+## Question 3
+## Making a separate column for genus extracted from Scientific Name
+NCandSC[,c("Genus")] = str_extract(NCandSC$ScientificName, "^[:alpha:]+\\b")
+
+## Updating NCandSC to be sorted by AvgCdia (m) in descending order
+NCandSC = arrange(NCandSC, desc(`AvgCdia (m)`))
+
+## Grouping and summarizing to find average crown size per genus
+avg_crown = group_by(NCandSC, Genus) %>% 
+  summarize(MeanCrownSize = mean(`AvgCdia (m)`))
+## Arranding in descending order
+avg_crown = arrange(avg_crown, desc(MeanCrownSize))
